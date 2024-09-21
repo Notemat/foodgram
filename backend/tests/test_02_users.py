95b4626746a7a9827c07bd2bc427a70c729fa06a
Никита Козлов <kozlov.nik@icloud.com>
2024-09-21 10:10:08 +0300

    add detail profile tests

--- backend/tests/test_02_users.py
+++ backend/tests/test_02_users.py
@@ -23,38 +23,78 @@ class TestUser:
             'password': 'Qwerty123'
         }
 
-    def test_successful_login(self, client, user_data):
+    @pytest.fixture(autouse=True)
+    def authenticated_client(self):
+        self.client = APIClient()
+        self.authenticated_data = {
+            'email': 'test_user_02@yandex.ru',
+            'username': 'test_user_02',
+            'first_name': 'Иван',
+            'last_name': 'Васильев',
+            'password': 'Qwerty321'
+        }
+        self.client.post(
+            self.URL_SIGNUP, self.authenticated_data)
+        login_response = self.client.post(
+            self.URL_GET_LOGIN,
+            {
+                'email': self.authenticated_data['email'],
+                'password': self.authenticated_data['password']
+            }
+        )
+        token = login_response.data['auth_token']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
+        return self.client
+
+    def test_successful_login(self):
         """Проверяем возможность авторизации с корректными данными."""
-        self.client.post(self.URL_SIGNUP, user_data)
         response = self.client.post(
             self.URL_GET_LOGIN,
-            {'email': user_data['email'], 'password': user_data['password']}
+            {
+                'email': self.user_data['email'],
+                'password': self.user_data['password']
+            }
         )
         assert response.status_code == 200
         assert 'auth_token' in response.data
 
-    def test_login_with_invalid_password(self, client, user_data):
+    def test_login_with_invalid_password(self):
         """Проверяем возможность авторизации с некорректными данными."""
-        self.client.post(self.URL_SIGNUP, user_data)
         response = self.client.post(
             self.URL_GET_LOGIN,
-            {'email': user_data['email'], 'password': self.WRONG_PASSWORD}
+            {'email': self.user_data['email'], 'password': self.WRONG_PASSWORD}
         )
         assert response.status_code == 401
         assert 'detail' in response.data
 
-    def test_successful_logout(self, client, user_data):
+    def test_successful_logout(self, authenticated_client):
         """Проверяем возможность удаления токена с корректными данными."""
-        self.client.post(self.URL_SIGNUP, user_data)
-        login_response = self.client.post(
-            self.URL_GET_LOGIN,
-            {'email': user_data['email'], 'password': user_data['password']}
-        )
-        token = login_response.data['auth_token']
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
-        logout_response = self.client.post(self.URL_GET_LOGOUT)
+        logout_response = authenticated_client.post(self.URL_GET_LOGOUT)
         assert logout_response.status_code == 204
 
-        self.client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
         failed_response = self.client.get(self.USER_ME_URL)
         assert failed_response.status_code == 401
+
+    def test_get_user_profile(self):
+        """Проверяем возможность получения информации о пользователе. """
+        register_response = self.client.post(self.URL_SIGNUP, self.user_data)
+        user_id = register_response.data['id']
+        response = self.client.get(f'{self.URL_SIGNUP}{user_id}/')
+        assert response.status_code == 200
+        assert response.data['username'] == self.user_data['username']
+        assert response.data['email'] == self.user_data['email']
+
+    def test_get_me_profile(self, authenticated_client):
+        """Проверяем возможность получения информации о своем профиле. """
+        response = authenticated_client.get(self.USER_ME_URL)
+        assert response.status_code == 200
+        assert response.data['username'] == self.authenticated_data['username']
+        assert response.data['email'] == self.authenticated_data['email']
+
+    def test_not_authenticated_get_me_profile(self):
+        """
+        Проверяем получение информации о своем профиле
+        неавторизированным пользователем.
+        """
+        response = self.client.get(self.USER_ME_URL)
+        assert response.status_code == 401
