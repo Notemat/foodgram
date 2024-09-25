f691db2746ca51184f3dd982939066143b22f183
Никита Козлов <kozlov.nik@icloud.com>
2024-09-25 17:20:38 +0300

    workflow migrations

--- backend/tests/test_06_subscriptions.py
+++ backend/tests/test_06_subscriptions.py
@@ -31,14 +31,14 @@ class TestSubscriptions:
         )
 
     @pytest.fixture
-    def create_01_subscription(self, second_user_id):
+    def create_subscription(self, second_user_id):
         """Создаем подписку на пользователя."""
         response = self.authenticated_client.post(
             f"{USER_URL}{second_user_id}{self.SUBSCRIBE_URL}"
         )
         return response
 
-    def test_02_subscribe_user(self, second_user_id):
+    def test_subscribe_user(self, second_user_id):
         """Проверяем возможность подписаться на пользователя."""
         response = self.authenticated_client.post(
             f"{USER_URL}{second_user_id}{self.SUBSCRIBE_URL}"
@@ -52,7 +52,7 @@ class TestSubscriptions:
         )
         assert response.data["is_subscribed"] is True
 
-    def test_03_not_subscribe_himself(self, first_user_id):
+    def test_01_not_subscribe_himself(self, first_user_id):
         """Проверяем невозможность подписаться на самого себя."""
         response = self.authenticated_client.post(
             f"{USER_URL}{first_user_id}{self.SUBSCRIBE_URL}"
@@ -60,7 +60,7 @@ class TestSubscriptions:
         assert response.status_code == 400
         assert "errors" in response.data
 
-    def test_04_unauthorized_user_cant_subscribe(self, client, second_user_id):
+    def test_02_unauthorized_user_cant_subscribe(self, client, second_user_id):
         """
         Проверяем, что неавторизованный пользователь не может подписаться.
         """
@@ -70,7 +70,7 @@ class TestSubscriptions:
         assert response.status_code == 401
         assert "detail" in response.data
 
-    def test_05_subscribe_twice(self, create_subscription, second_user_id):
+    def test_03_subscribe_twice(self, create_subscription, second_user_id):
         """
         Проверяем, что невозможно подписаться дважды
         на одного пользователя.
@@ -83,13 +83,13 @@ class TestSubscriptions:
         assert second_response.status_code == 400
         assert "errors" in second_response.data
 
-    def test_06_get_subscriptions(self, create_subscription):
+    def test_04_get_subscriptions(self, create_subscription):
         """Проверяем доступность списка подписок."""
         response = self.authenticated_client.get(self.SUBSCRIPTION_URL)
         assert response.status_code == 200
         assert "results" in response.data
 
-    def test_07_unauthorized_user_cant_get_subscriptions(self, client):
+    def test_05_unauthorized_user_cant_get_subscriptions(self, client):
         """
         Проверяем, что неавторизованный пользователь
         не сможет подписаться.
@@ -98,7 +98,7 @@ class TestSubscriptions:
         assert response.status_code == 401
         assert "detail" in response.data
 
-    def test_08_delete_subscribe(self, create_subscription, second_user_id):
+    def test_06_delete_subscribe(self, create_subscription, second_user_id):
         """Проверяем возможность отписаться от пользователя."""
         response = self.authenticated_client.delete(
             f"{USER_URL}{second_user_id}{self.SUBSCRIBE_URL}"
@@ -106,7 +106,7 @@ class TestSubscriptions:
         assert response.status_code == 204
         assert response.data is None
 
-    def test_09_delete_not_subscriber(self, second_user_id):
+    def test_07_delete_not_subscriber(self, second_user_id):
         """
         Проверяем, что нельзя отписаться от пользователя
         на которого не был подписан.
@@ -117,7 +117,7 @@ class TestSubscriptions:
         assert response.status_code == 400
         assert "errors" in response.data
 
-    def test_10_unauthorized_user_cant_delete(self, client, second_user_id):
+    def test_08_unauthorized_user_cant_delete(self, client, second_user_id):
         """
         Проверяем, что неавторизованный пользователь
         не может отписываться.
