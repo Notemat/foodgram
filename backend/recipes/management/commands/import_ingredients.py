233b512bc1e9b0ebfdafdf89533d2dc8ac39134b
Никита Козлов <kozlov.nik@icloud.com>
2024-09-26 22:29:04 +0300

    final deploy I hope

--- backend/recipes/management/commands/import_ingredients.py
+++ backend/recipes/management/commands/import_ingredients.py
@@ -1,4 +1,3 @@
-import csv
 import json
 
 from django.core.management.base import BaseCommand
@@ -10,24 +9,13 @@ class Command(BaseCommand):
     """Импортируем ингредиенты."""
 
     def handle(self, *args, **kwargs):
-        self.import_csv()
         self.import_json()
 
-    def import_csv(self):
-        with open(
-            'data/ingredients.csv', newline='', encoding='utf-8'
-        ) as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                Ingredient.objects.get_or_create(
-                    name=row['name'], measurement_unit=row['measurement_unit']
-                )
-
     def import_json(self):
-        with open('data/ingredients.json', 'r', encoding='utf-8') as jsonfile:
-            ingredients = json.load(jsonfile)
-            for ingredient in ingredients:
+        with open('data/ingredients.json', encoding='utf-8') as file:
+            data = json.load(file)
+            for item in data:
                 Ingredient.objects.get_or_create(
-                    name=ingredient['name'],
-                    measurement_unit=ingredient['measurement_unit']
+                    name=item['name'],
+                    measurement_unit=item['measurement_unit']
                 )
