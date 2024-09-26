9cdd3be941164cf37f44100e336c35ca8d67d07e
Никита Козлов <kozlov.nik@icloud.com>
2024-09-26 09:41:12 +0300

    fix nginx and urls

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -4,7 +4,7 @@ from recipes.views import RedirectToRecipeView
 
 urlpatterns = [
     path(
-        "s/<str:short_link>/",
+        "<str:short_link>/",
         RedirectToRecipeView.as_view(),
         name="short_link_redirect",
     ),
