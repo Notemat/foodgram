b33c2573cd6e0fbf0632a6aac8f49e1bcfc66628
Никита Козлов <kozlov.nik@icloud.com>
2024-09-26 17:00:18 +0300

    nginx

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -10,19 +10,3 @@ class RedirectToRecipeView(View):
     def get(self, request, short_link):
         recipe = get_object_or_404(Recipe, short_link=short_link)
         return redirect(f"/recipes/{recipe.id}/")
-
-
-# class ShortLinkView(APIView):
-#     """Вью для получения короткой ссылки на рецепт."""
-
-#     def get(self, request, pk=None):
-#         """Получаем короткую ссылку на рецепт."""
-#         recipe = get_object_or_404(Recipe, pk=pk)
-#         short_link = recipe.short_link
-#         if not short_link:
-#             short_link = recipe.generate_short_link()
-#             recipe.short_link = short_link
-#             recipe.save()
-
-#         link = request.build_absolute_uri(f"{short_link}")
-#         return Response({"short-link": link}, status=status.HTTP_200_OK)
